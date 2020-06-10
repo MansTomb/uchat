3797c7272a8cc7409d15e478107df65ac01ea6e5
Anton Balabin <abalabin@e1r3p1.unit.ua>
2020-06-10 17:22:07 +0300

    contacts start

---
+++ Client/src/contacts/constructor.c
@@ -0,0 +1,10 @@
+#include "client.h"
+
+// t_profile *mx_profile_constructor(t_info *info) {
+//     t_profile *new = malloc(sizeof(t_profile));
+
+//     if (new) {
+
+//     }
+//     return new;
+// }
