a6a48698a6810c384a9bcf0485787c4029660964
wizaral <alex.macromaster@outlook.com>
2020-06-15 01:41:51 +0300

    delete db, add more scripts

--- Server/db/scripts.sql
+++ Server/db/scripts.sql
@@ -3,7 +3,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     login VARCHAR(32) NOT NULL UNIQUE,
-    salt VARCHAR(64) NOT NULL UNIQUE,
     hash VARCHAR(64) NOT NULL
 );
 
@@ -11,37 +10,35 @@ CREATE TABLE IF NOT EXISTS users_profiles (
     user_id INTEGER PRIMARY KEY NOT NULL,
     first_name VARCHAR(64),
     second_name VARCHAR(64),
-    status VARCHAR(32),
-    register_time CHAR(19)
+    status VARCHAR(32)
 );
 
 CREATE TRIGGER IF NOT EXISTS add_profile AFTER INSERT
     ON users
 BEGIN
-    INSERT INTO users_profiles (user_id, register_time) VALUES
-        (NEW.id, datetime('now', 'localtime'));
+    INSERT INTO users_profiles (user_id) VALUES (NEW.id);
 END;
 
 CREATE TRIGGER IF NOT EXISTS del_profile AFTER DELETE
     ON users
 BEGIN
-    DELETE FROM users_profiles
-    WHERE user_id = OLD.id;
+    DELETE FROM users_profiles WHERE user_id = OLD.id;
 END;
 
 -- user contacts block
 
-CREATE TABLE IF NOT EXISTS contact_groups (
+CREATE TABLE IF NOT EXISTS contacts_groups (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name VARCHAR(64) NOT NULL UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS contact_lists (
+CREATE TABLE IF NOT EXISTS contacts_lists (
     user_id INTEGER NOT NULL,
     contact_id INTEGER NOT NULL,
     group_id INTEGER,
 
-    PRIMARY KEY (user_id, contact_id)
+    PRIMARY KEY (user_id, contact_id),
+    CHECK (user_id != contact_id)
 );
 
 -- chats block
@@ -52,12 +49,12 @@ CREATE TABLE IF NOT EXISTS chats (
     name VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS user_chats (
+CREATE TABLE IF NOT EXISTS users_chats (
     user_id INTEGER NOT NULL,
     chat_id INTEGER NOT NULL,
-    role INTEGER DEFAULT 1,
+    role INTEGER DEFAULT 1 NOT NULL,
 
-    PRIMARY KEY (user_id, char_id)
+    PRIMARY KEY (user_id, chat_id)
 );
 
 CREATE TABLE IF NOT EXISTS messages (
@@ -69,27 +66,99 @@ CREATE TABLE IF NOT EXISTS messages (
     content VARCHAR(64) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS block_lists (
-    chat_id INTEGER NOT NULL,
-    banned_id INTEGER NOT NULL,
-
-    PRIMARY KEY (chat_id, banned_id)
-);
-
--- register users
-
-INSERT INTO users VALUES
-    (NULL, 'abalabin', 'qwerty', 'qwerty'),
-    (NULL, 'oafanasiev', 'asdfgh', 'asdfgh'),
-    (NULL, 'trohalska', 'zxcvbn', 'zxcvbn'),
-    (NULL, 'yburienkov', 'lpmkoj', 'lpmkoj');
-
+-- проверка работы триггеров
 SELECT * FROM users;
 SELECT * FROM users JOIN users_profiles ON id = user_id;
+DELETE FROM users;
 
--- UPDATE users_profiles SET first_name = (SELECT login FROM users WHERE users.id = users_profiles.user_id);
--- UPDATE users_profiles SET second_name = (SELECT login FROM users WHERE users.id = users_profiles.user_id);
--- UPDATE users_profiles SET status = (SELECT login FROM users WHERE users.id = users_profiles.user_id);
+INSERT INTO users VALUES
+    (NULL, 'abalabin', 'qwerty'),
+    (NULL, 'oafanasiev', 'asdfgh'),
+    (NULL, 'trohalska', 'zxcvbn'),
+    (NULL, 'yburienkov', 'lpmkoj');
+
+-- регистрация
+INSERT INTO users VALUES (NULL, 'NEW_USER_LOGIN', 'NEW_USER_PASSWORD');
+
+-- авторизация
+SELECT * FROM users WHERE login = 'NEW_USER_LOGIN';
+
+-- удаление юзера
+DELETE FROM users WHERE id = USER_ID;
+
+-- изменение пароля
+UPDATE users SET hash = 'STRONG_PASSWORD' WHERE id = USER_ID;
+
+-- получение профиля
+SELECT * FROM users_profiles WHERE user_id = USER_ID;
+
+-- обновление профиля
+UPDATE users_profiles SET first_name = 'NEW_FIRST_NAME' WHERE user_id = USER_ID;
+UPDATE users_profiles SET second_name = 'NEW_SECOND_NAME' WHERE user_id = USER_ID;
+UPDATE users_profiles SET status = 'NEW_STATUS' WHERE user_id = USER_ID;
+
+-- создание новой группы контактов
+INSERT INTO contacts_groups VALUES (NULL, 'NEW_GROUP_NAME');
+
+-- добавление нового контакта
+INSERT INTO contacts_lists VALUES (USER_ID, CONTACT_ID, NULL);
+
+-- удаление из контактов
+DELETE FROM contacts_lists WHERE user_id = USER_ID AND contact_id = CONTACT_ID;
+
+-- добавление контакта в группу
+UPDATE contacts_lists SET group_id = GROUP_ID WHERE user_id = USER_ID AND contact_id = CONTACT_ID;
+
+-- удаление контакта из группы
+UPDATE contacts_lists SET group_id = NULL WHERE user_id = USER_ID AND contact_id = CONTACT_ID;
+
+-- создание чата
+INSERT INTO chats VALUES (NULL, CHAT_TYPE, 'NEW_CHAT_NAME');
+
+-- переименование группового чата или канала
+UPDATE chats SET name = 'CHAT_NEW_NAME' WHERE id = CHAT_ID AND type > 1;
+
+-- добавление юзера в чат
+INSERT INTO users_chats VALUES (USER_ID, CHAT_ID);
+
+-- изменение роли (бан / выход / новый админ / новый создатель)
+UPDATE users_chats SET role = NEW_ROLE WHERE user_id = USER_ID AND chat_id = CHAT_ID;
+
+-- отправка сообщения
+INSERT INTO messages VALUES (NULL, USER_ID, CHAT_ID, TYPE, datetime('now', 'localtime'), CONTENT);
+
+-- удаление сообщения
+DELETE FROM messages WHERE id = MESSAGE_ID;
+
+-- -- -- -- -- -- -- -- -- -- комплексные запросы -- -- -- -- -- -- -- -- -- --
+
+-- создание личной переписки
+INSERT INTO chats VALUES (NULL, 1, '');
+INSERT INTO users_chats VALUES (USER_ID1, last_insert_rowid(), 1);
+INSERT INTO users_chats VALUES (USER_ID2, (SELECT max(id) FROM chats), 1);
+
+-- создание нового группового чата / канала
+INSERT INTO chats VALUES (NULL, CHAT_TYPE, 'NEW_CHAT_NAME');
+INSERT INTO users_chats VALUES (USER_ID, last_insert_rowid(), 3);
+
+-- выборка контактов юзера при загрузке клиента
+SELECT contact_id, login, first_name, second_name, status, group_id, name
+FROM contacts_lists AS cl
+    JOIN users AS u ON cl.contact_id = u.id AND cl.user_id = USER_ID
+    JOIN users_profiles AS up ON cl.contact_id = up.user_id
+    LEFT JOIN contacts_groups AS cg ON cl.group_id = cg.id;
+
+-- выборка чатов юзера при загрузке клиента
+SELECT chat_id, role, type, name
+FROM users_chats AS uc
+    JOIN chats AS c ON uc.chat_id = c.id AND uc.user_id = USER_ID;
+
+-- выборка сообщений в чате при загрузке клиента
+SELECT * FROM messages WHERE chat_id = CHAT_ID ORDER BY id DESC LIMIT 50;
+
+-- отправка сообщения
+INSERT INTO messages VALUES (NULL, USER_ID, CHAT_ID, TYPE, datetime('now', 'localtime'), CONTENT);
+SELECT user_id FROM users_chats WHERE chat_id = CHAT_ID AND user_id != USER_ID;
 
 -- .headers on
 -- .mode column
