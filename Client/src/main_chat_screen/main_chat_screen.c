a4c800eb91a55042f72ed61f2ba15823f522b895
Anton Balabin <abalabin@e1r3p1.unit.ua>
2020-06-15 15:04:04 +0300

    register password match check

--- Client/src/main_chat_screen/main_chat_screen.c
+++ Client/src/main_chat_screen/main_chat_screen.c
@@ -13,25 +13,25 @@ void mx_main_chat_screen_show(t_info *info) {
     //подтянуть чаты но пока заглушка -----------------
     mx_push_back(info->chat_list, mx_chat_constructor(info));
     mx_push_back(info->chat_list, mx_chat_constructor(info));
-    // mx_push_back(info->chat_list, mx_chat_constructor(info));
+    mx_push_back(info->chat_list, mx_chat_constructor(info));
 
     t_message *one = mx_message_construct(info, text, "Loh1");
     t_message *one2 = mx_message_construct(info, text2, "Loh1");
-    // t_message *one3 = mx_message_construct(info, text2, "Heh");
+    t_message *one3 = mx_message_construct(info, text2, "Heh");
     t_message *one4 = mx_message_construct(info, text2, "Heh");
     t_message *one5 = mx_message_construct(info, text2, "Heh");
-    // t_message *one6 = mx_message_construct(info, text2, "blyat");
+    t_message *one6 = mx_message_construct(info, text2, "blyat");
 
     mx_chat_switcher_add_chat(info, (t_chat *)mx_get_index(info->chat_list, 0)->data, "general");
     mx_chat_switcher_add_chat(info, (t_chat *)mx_get_index(info->chat_list, 1)->data, "new1");
-    // mx_chat_switcher_add_chat(info, (t_chat *)mx_get_index(info->chat_list, 2)->data, "new2");
+    mx_chat_switcher_add_chat(info, (t_chat *)mx_get_index(info->chat_list, 2)->data, "new2");
 
     mx_chat_message_put((t_chat *)mx_get_index(info->chat_list, 0)->data, one);
     mx_chat_message_put((t_chat *)mx_get_index(info->chat_list, 1)->data, one2);
     mx_chat_message_put((t_chat *)mx_get_index(info->chat_list, 1)->data, one4);
     mx_chat_message_put((t_chat *)mx_get_index(info->chat_list, 1)->data, one5);
-    // mx_chat_message_put((t_chat *)mx_get_index(info->chat_list, 2)->data, one6);
-    // mx_chat_message_put((t_chat *)mx_get_index(info->chat_list, 2)->data, one3);
+    mx_chat_message_put((t_chat *)mx_get_index(info->chat_list, 2)->data, one6);
+    mx_chat_message_put((t_chat *)mx_get_index(info->chat_list, 2)->data, one3);
     // ---------------------------------------------------------------------------- это всё заглушка
     mx_main_menu_show(info);
     mx_chat_switcher_show(info);
--- Client/src/register/errors.c
+++ Client/src/register/errors.c
@@ -5,6 +5,7 @@ static gboolean validate_entry(t_register *reg) {
         && mx_entry_text_exist(reg->password_entry)
         && mx_entry_text_exist(reg->password2_entry))
         return true;
+    mx_dialog_warning_create(NULL, "All fields must not be empty!");
     return false;
 }
 
@@ -19,21 +20,29 @@ static gboolean validate_entry_chars(GtkWidget *entry) {
     return true;
 }
 
-gboolean mx_reg_data_validate(t_register *reg) {
-    if (validate_entry(reg)) {
-        if (validate_entry_chars(reg->username_entry)
+static gboolean password_match(t_register *reg) {
+    const char *text1 = mx_entry_get_text(reg->password_entry);
+    const char *text2 = mx_entry_get_text(reg->password2_entry);
+
+    if (!strcmp(text1, text2))
+        return true;
+    mx_dialog_warning_create(NULL, "Password must match!");
+    return false;
+}
+
+static gboolean validate_entryes(t_register *reg) {
+    if (validate_entry_chars(reg->username_entry) 
             && validate_entry_chars(reg->password_entry)
-            && validate_entry_chars(reg->password2_entry)) {
+            && validate_entry_chars(reg->password2_entry))
+            return true;
+    mx_dialog_warning_create(NULL, "Username and password must be printable!");
+    return false;
+}
+
+gboolean mx_reg_data_validate(t_register *reg) {
+    if (validate_entry(reg))
+        if (validate_entryes(reg))
+            if (password_match(reg))
                 return true;
-            }
-        else {
-            mx_dialog_warning_create(NULL, 
-                                    "All fields chars must be printable");
-            return false;
-        }
-    }
-    else {
-        mx_dialog_warning_create(NULL, "All fields must not be empty!");
-        return false;
-    }
+    return false;
 }
