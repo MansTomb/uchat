75f1d40852a8a99c01343fe2069bba894490c48f
ronald112 <ronald11211@gmail.com>
2020-06-18 15:20:45 +0300

    add free for string, change names for enum

--- Client/src/login/send_json_login.c
+++ Client/src/login/send_json_login.c
@@ -4,13 +4,15 @@ static void login_build_json(const char *login, const char *password, int s_sock
     cJSON *jlogin = cJSON_CreateObject();
     char *string = NULL;
 
-    cJSON_AddNumberToObject(jlogin, "json_type", mx_make_authorization);
+    cJSON_AddNumberToObject(jlogin, "json_type", make_authorization);
     cJSON_AddStringToObject(jlogin, "password", password);
     cJSON_AddStringToObject(jlogin, "login", login);
 
     string = cJSON_Print(jlogin);
     send(s_sock, string, sizeof(string), 0);
     cJSON_Delete(jlogin);
+    if (MX_MALLOC_SIZE(string))
+        free(string);
 }
 
 void mx_login_build_json_wrapper(t_info *info) {
--- Client/src/register/send_json_reg.c
+++ Client/src/register/send_json_reg.c
@@ -4,13 +4,15 @@ static void reg_bld_json(const char *login, const char *password, int s_sock) {
     cJSON *jlogin = cJSON_CreateObject();
     char *string = NULL;
 
-    cJSON_AddNumberToObject(jlogin, "json_type", mx_make_register);
+    cJSON_AddNumberToObject(jlogin, "json_type", make_register);
     cJSON_AddStringToObject(jlogin, "password", password);
     cJSON_AddStringToObject(jlogin, "login", login);
 
     string = cJSON_Print(jlogin);
     send(s_sock, string, sizeof(string), 0);
     cJSON_Delete(jlogin);
+    if (MX_MALLOC_SIZE(string))
+        free(string);
 }
 
 void mx_register_build_json_wrapper(t_info *info) {
