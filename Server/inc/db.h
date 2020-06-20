789a667bd8d0d2a21d020f8317980922c8b1df85
Tetiana Rohalska <55732200+trohalska@users.noreply.github.com>
2020-06-20 11:04:10 +0300

    merge Server

--- Server/inc/db.h
+++ Server/inc/db.h
@@ -5,26 +5,44 @@ typedef enum {
     make_register,
     failed_register,
     success_register,
+
     make_authorization,
     failed_authorization,
     success_authorization,
+
     make_deletion,
     failed_deletion,
     success_deletion,
+
     make_change_password,
     failed_change_password,
     success_change_password,
+
     make_update_profile,
     success_update_profile,
+
     get_client_data,
     send_client_data,
+
     get_client_contacts,
     send_client_contacts,
+
     get_client_chats,
     send_client_chats,
+
     get_client_messages,
     send_client_messages,
-    make_new_chat,
+
+    make_new_personal_chat,
+    failed_new_personal_chat,
+    success_new_personal_chat,
+
+    make_new_group_chat,
+    get_new_group_chat,
+
+    make_new_chanel,
+    get_new_chanel,
+
     send_message,
 } t_actions;
 
@@ -34,5 +52,8 @@ int mx_create_db(sqlite3 *db);
 int mx_init_db(sqlite3 **db);
 void mx_close_db(sqlite3 *db);
 
-cJSON *mx_registration(sqlite3 *db, cJSON *reg);
-cJSON *mx_authorization(sqlite3 *db, cJSON *reg);
+cJSON *mx_registration(sqlite3 *db, cJSON *jsn);
+cJSON *mx_authorization(sqlite3 *db, cJSON *jsn);
+
+cJSON *mx_create_personal_chat(sqlite3 *db, cJSON *jsn);
+void mx_get_present_chat(sqlite3 *db, cJSON *jsn);
--- Server/src/sql/test.c
+++ Server/src/sql/test.c
@@ -38,11 +38,32 @@ static void test_autorization(sqlite3 *db) {
     cJSON_Delete(obj);
 }
 
+static void test_new_pchat(sqlite3 *db) {
+    cJSON *obj = cJSON_CreateObject();
+    cJSON_AddNumberToObject(obj, "json_type", make_new_personal_chat);
+    cJSON_AddStringToObject(obj, "uid1", "1");
+    cJSON_AddStringToObject(obj, "uid2", "2");
+
+    cJSON *res = mx_create_personal_chat(db, obj);
+    int type = cJSON_GetObjectItem(obj, "json_type")->valueint;
+
+    printf("%i\n", type);
+
+    if (type == success_new_personal_chat) {
+        printf("SUCCESS = %i!!!\n", cJSON_GetObjectItem(obj, "id")->valueint);
+    }
+    else {
+        printf("FAILED!!!\n");
+    }
+    cJSON_Delete(obj);
+}
+
 int main() {
     sqlite3 *db = NULL;
     mx_init_db(&db);
 
-    test_register(db);
-    test_autorization(db);
+    // test_register(db);
+    // test_autorization(db);
+    test_new_pchat(db);
     mx_close_db(db);
 }
