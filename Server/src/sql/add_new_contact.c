bb344d30b1e4fcfdb1d54595d310fdb49f7ae438
Oleksandr Afanasiev <oafanasiev@e1r3p5.unit.ua>
2020-07-04 13:41:32 +0300

    fix adding contact

--- Server/src/sql/add_new_contact.c
+++ Server/src/sql/add_new_contact.c
@@ -1,14 +1,6 @@
 #include "server.h"
 
-static int callback(void *data, int argc, char **argv, char **cols) {
-    if (!(strcmp(argv[0], "1"))) {
-        cJSON_AddNumberToObject(data, "coid", atoi(argv[1]));
-        return false;
-    }
-    return true;
-}
-
-static void add_new_contact(sqlite3 *db, cJSON *jsn) {
+cJSON *mx_add_new_contact(sqlite3 *db, cJSON *jsn) {
     char *query = NULL;
     char *err = NULL;
     int rc = 0;
@@ -20,25 +12,8 @@ static void add_new_contact(sqlite3 *db, cJSON *jsn) {
     if (mx_check(rc, err, "adding new contact") != SQLITE_OK) {
         MX_SET_TYPE(jsn, failed_add_new_contact);
     }
-    free(query);
-}
-
-cJSON *mx_add_new_contact(sqlite3 *db, cJSON *jsn) {
-    char *query = NULL;
-    char *err = NULL;
-    int rc = 0;
-
-    // should take coid = contact id
-    asprintf(&query, "SELECT count(*), id FROM users WHERE login = '%s';",
-            MX_VSTR(jsn, "friend_login"));
-    rc = sqlite3_exec(db, query, callback, jsn, &err);
-
-    if (mx_check(rc, err, "add new contact") != SQLITE_OK) {
-        MX_SET_TYPE(jsn, failed_add_new_contact);
-    }
     else {
         MX_SET_TYPE(jsn, success_add_new_contact);
-        add_new_contact(db, jsn);
     }
     free(query);
     return jsn;
