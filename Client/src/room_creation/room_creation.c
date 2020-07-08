c2fc39ec4879515f4108ae271b7d4c1acddc955d
Anton Balabin <abalabin@e1r3p1.unit.ua>
2020-07-08 12:28:47 +0300

    room creation callbacks

--- Client/src/room_creation/room_creation.c
+++ Client/src/room_creation/room_creation.c
@@ -25,37 +25,3 @@ void mx_room_creation_destroy(t_info *info) {
     free(info->windows->rc);
     info->windows->rc = NULL;
 }
-
-// void mx_on_click_create_room_channel(GtkWidget *widget, gpointer data) {
-//     t_info *info = data;
-//     t_room_creation *dialog = info->windows->room_creation;
-
-//     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(dialog->groupbt), FALSE);
-// }
-
-// void mx_on_click_create_room_group(GtkWidget *widget, gpointer data) {
-//     t_info *info = data;
-//     t_room_creation *dialog = info->windows->room_creation;
-
-//     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(dialog->channelbt), FALSE);
-// }
-
-// void mx_on_click_create_room_create(GtkWidget *widget, gpointer data) {
-//     t_info *info = data;
-//     t_room_creation *dialog = info->windows->room_creation;
-
-    
-//     if (mx_room_creation_data_validation(dialog)) {
-//         // action "mx_make_new_chat" estimated return "mx_make_new_chat_success"(TODO)
-//         mx_room_creation_destructor(info);
-//     }
-// }
-
-// void mx_on_click_create_room_cancel(GtkWidget *widget, gpointer data) {
-//     t_info *info = data;
-//     t_room_creation *dialog = info->windows->room_creation;
-
-//     mx_room_creation_destructor(info);
-// }
-
-
