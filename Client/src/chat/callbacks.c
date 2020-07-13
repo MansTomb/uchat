621665be4cada2ccfe0d17ad0776ac6279a10ac4
Anton Balabin <abalabin@e1r3p1.unit.ua>
2020-07-13 12:55:09 +0300

    chat send callbacks ready for you YURA

--- Client/src/chat/callbacks.c
+++ Client/src/chat/callbacks.c
@@ -23,49 +23,34 @@ static void delete_text(t_chat *chat) {
 
 void mx_send_message(GtkWidget *widget, gpointer data) {
     t_chat *chat = data;
-    t_message *msg = mx_message_build(chat->info, "4Elovek", get_text(chat));
+    t_profile_data *prof = chat->info->cl_data->profile;
+
+    // prof->id;
+    // chat->cid;
+    // type = 1;
+    // content = get_text(chat);
+
+    /*
+        Твоя фун-ка здесь юра, сверху написано как достать всё что надо для джсон
+        Советую в твою ф-ку передать chat, а из него уже в инфо идти бо иначе
+        ты не сможешь в get_text(<сюда>) правильный чат
+    */
 
     delete_text(chat);
-    gtk_list_box_insert(GTK_LIST_BOX(chat->message_box), msg->main_fixed, -1);
-}
-
-gboolean mx_on_key_press(GtkWidget *widget, GdkEvent *event, gpointer data) {
-    t_chat *chat = data;
-
-    if (event->key.keyval == GDK_KEY_Return && !(event->key.state & GDK_SHIFT_MASK)) {
-        mx_send_message(widget, data);
-        return TRUE;
-    }
-    return FALSE;
 }
 
 void mx_chat_img_send(GtkWidget *widget, gpointer data) {
     t_chat *chat = data;
-    GtkWidget *img = gtk_image_new_from_animation(gdk_pixbuf_animation_new_from_file(gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(chat->img_dialog)), NULL));
-    t_message_img *msg = mx_message_img_build(chat->info, "4Elovek", img);
+    t_profile_data *prof = chat->info->cl_data->profile;
 
-    gtk_list_box_insert(GTK_LIST_BOX(chat->message_box), msg->main_fixed, -1);
+    // prof->id;
+    // chat->cid;
+    // type = 1;
+    // content = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(chat->img_dialog);
+
+    /*
+        В контенте ебу как там файлы передаються вообмещ ф-кой сверху сможешь
+        найти путь к файлу
+    */
     gtk_widget_hide(chat->img_dialog);
-}
-
-void mx_choose_dialog(GtkWidget *widget, gpointer data) {
-    t_chat *chat = data;
-
-    gtk_widget_show(chat->img_dialog);
-}
-
-void mx_on_dialog_cancel(GtkWidget *widget, gpointer data) {
-    t_chat *chat = data;
-
-    gtk_widget_hide(chat->img_dialog);
-}
-
-void mx_on_scroll_edge(GtkWidget *widget, GtkPositionType pos, gpointer data) {
-    t_chat *chat = data;
-
-    printf("Load messages netop\n");
-    if (GTK_POS_TOP == pos) {
-        printf("Load messages\n");
-        /* Load message */
-    }
 }
---
+++ Client/src/chat/callbacks_2.c
@@ -0,0 +1,32 @@
+#include "client.h"
+
+gboolean mx_on_key_press(GtkWidget *widget, GdkEvent *event, gpointer data) {
+    t_chat *chat = data;
+
+    if (event->key.keyval == GDK_KEY_Return && !(event->key.state & GDK_SHIFT_MASK)) {
+        mx_send_message(widget, data);
+        return TRUE;
+    }
+    return FALSE;
+}
+void mx_choose_dialog(GtkWidget *widget, gpointer data) {
+    t_chat *chat = data;
+
+    gtk_widget_show(chat->img_dialog);
+}
+
+void mx_on_dialog_cancel(GtkWidget *widget, gpointer data) {
+    t_chat *chat = data;
+
+    gtk_widget_hide(chat->img_dialog);
+}
+
+void mx_on_scroll_edge(GtkWidget *widget, GtkPositionType pos, gpointer data) {
+    t_chat *chat = data;
+
+    printf("Load messages netop\n");
+    if (GTK_POS_TOP == pos) {
+        printf("Load messages\n");
+        /* Load message */
+    }
+}
