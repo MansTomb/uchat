785ca8a95c15167f9b199ceba434386e7ec62633
Anton Balabin <abalabin@e1r3p1.unit.ua>
2020-07-23 12:39:39 +0300

    fixes

--- Client/src/handlers/handle_invite_user.c
+++ Client/src/handlers/handle_invite_user.c
@@ -3,7 +3,7 @@
 void mx_handle_invite_user(t_info *info, cJSON *json) {
     int cid = cJSON_GetObjectItem(json, "cid")->valueint;
     int uid = cJSON_GetObjectItem(json, "uid")->valueint;
-    int login = cJSON_GetObjectItem(json, "login")->valueint;
+    char *login = cJSON_GetObjectItem(json, "login")->valuestring;
     t_chat_member *member = malloc(sizeof(t_chat_member));
 
     member->uid = uid;
--- Client/src/handlers/read_non_requested_jsons.c
+++ Client/src/handlers/read_non_requested_jsons.c
@@ -12,8 +12,8 @@ bool mx_handle_if_not_requested(t_info *info, cJSON *json) {
         mx_handle_edit_message(info, json);
     else if (type == delete_message && (ret = 0))
         mx_handle_delete_message(info, json);
-    else if (type == invited_user)
-        mx_handle_invite_user(info, json);
+    // else if (type == invited_user)
+    //     mx_handle_invite_user(info, json);
     cJSON_Delete(json);
     return ret;
 }
