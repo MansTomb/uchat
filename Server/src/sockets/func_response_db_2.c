cf606589ad7dc74e7f6c5ac74257b7a9a4ce55ed
Tetiana Rohalska <55732200+trohalska@users.noreply.github.com>
2020-07-24 10:48:43 +0300

    ban-unban

--- Server/src/sockets/func_response_db_2.c
+++ Server/src/sockets/func_response_db_2.c
@@ -35,8 +35,8 @@ void mx_db_send_message(t_info *info, t_peer *peer, cJSON *get) {
     if (MX_TYPE(bd) == failed_send_message) {
         // printf("%s", cJSON_Print(bd));
         mx_send_message_handler(info->sock, peer, bd, peer->socket);
-        mx_response_db(info, peer, mx_server_msg(bd,
-                       "Вы забанены или не можете писать в этом чате!"));
+        mx_send_message_handler(info->sock, peer, mx_server_msg(bd,
+            " - не может писать в этом чате!"), peer->socket);
     }
     else {
         len = cJSON_GetArraySize(cJSON_GetObjectItem(bd, "clients_id"));
