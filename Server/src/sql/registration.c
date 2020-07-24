5a468f4cc7dff8962514658710270a1c69ac1003
wizaral <alexafa@ukr.net>
2020-07-24 11:50:28 +0300

    auto add to super-chanel

--- Server/src/sql/registration.c
+++ Server/src/sql/registration.c
@@ -9,6 +9,14 @@ static int get_id(void *data, int argc, char **argv, char **cols) {
     return 0;
 }
 
+static void add_to_super_chanel(sqlite3 *db, cJSON *jsn) {
+    char *query = "INSERT INTO users_chats VALUES (last_insert_rowid(), 1,1);";
+    char *err = NULL;
+    int rc = sqlite3_exec(db, query, NULL, NULL, &err);
+
+    mx_check(rc, err, "add to super-chat");
+}
+
 static void accept_register(sqlite3 *db, cJSON *jsn) {
     char *query = NULL;
     char *err = NULL;
@@ -20,6 +28,9 @@ static void accept_register(sqlite3 *db, cJSON *jsn) {
     rc = sqlite3_exec(db, query, get_id, jsn, &err);
     if (mx_check(rc, err, "accepted registration") != SQLITE_OK) {
         MX_SET_TYPE(jsn, failed_register);
+    }
+    else {
+        MX_SET_TYPE(jsn, success_register);
     }
     free(query);
 }
@@ -36,8 +47,8 @@ cJSON *mx_registration(sqlite3 *db, cJSON *jsn) {
     if (mx_check(rc, err, "registration") != SQLITE_OK)
         MX_SET_TYPE(jsn, failed_register);
     else {
-        MX_SET_TYPE(jsn, success_register);
         accept_register(db, jsn);
+        add_to_super_chanel(db, jsn);
     }
     cJSON_DeleteItemFromObject(jsn, "login");
     cJSON_DeleteItemFromObject(jsn, "hash");
